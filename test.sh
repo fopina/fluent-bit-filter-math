97d1fed628d9894798191f2594f608960a982830
Filipe Pina <fopina@gmail.com>
2020-04-19 15:22:32 +0100

    copy from https://github.com/fluent/fluent-bit/tree/master/plugins/filter_nest as filter template (with "Operation" parameter)

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 docker run --rm \
            -v $(pwd):/myplugin fopina/fluent-bit-plugin-dev \
            cmake -DFLB_SOURCE=/usr/src/fluentbit/fluent-bit-1.4.2/ \
-                 -DPLUGIN_NAME=out_stdout2 ../
+                 -DPLUGIN_NAME=filter_math ../
 
 docker run --rm \
            -v $(pwd):/myplugin fopina/fluent-bit-plugin-dev \
@@ -15,4 +15,9 @@ docker run --rm \
 
 docker run --rm \
            -v $(pwd)/build:/myplugin fluent/fluent-bit:1.4.2 \
-           /fluent-bit/bin/fluent-bit -e /myplugin/flb-out_stdout2.so -i cpu -o stdout2
+           /fluent-bit/bin/fluent-bit \
+           -f 1 \
+           -e /myplugin/flb-filter_math.so \
+           -i mem \
+           -F math -p 'Operation=nest' -p 'Wildcard=Mem.*' -p 'Nest_under=Memstats' -p 'Remove_prefix=Mem.' -m '*' \
+           -o stdout
